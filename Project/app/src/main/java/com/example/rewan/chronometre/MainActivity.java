0440a3b22c296015d4664c9535cf2fd55ed5fe47
Erwan Chesnay <erwan.chesnay@etud.u-picardie.fr>
2018-02-03 11:06:00 +0100

    Config du UI et test click boutton

--- Project/app/src/main/java/com/example/rewan/chronometre/MainActivity.java
+++ Project/app/src/main/java/com/example/rewan/chronometre/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.rewan.chronometre;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    private EditText minute_text;
+    private EditText seconde_text;
+    private Button button_start;
+    private TextView arret;
+
+    private void bindViews() {
+        button_start = findViewById(R.id.button_start);
+        minute_text = findViewById(R.id.minute);
+        seconde_text = findViewById(R.id.seconde);
+        arret = findViewById(R.id.arret);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindViews();
+
+        button_start.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                arret.setText("lol");
+        }
+        });
     }
+
 }
