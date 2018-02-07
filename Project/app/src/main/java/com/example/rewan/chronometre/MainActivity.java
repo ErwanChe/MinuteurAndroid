bee7dffb5e1babf5df6be8b119056f82d1ee4e60
Erwan CHESNAY <erwan.chesnay@outlook.fr>
2018-02-07 15:23:33 +0100

    Pas loin de la fin

--- Project/app/src/main/java/com/example/rewan/chronometre/MainActivity.java
+++ Project/app/src/main/java/com/example/rewan/chronometre/MainActivity.java
@@ -4,23 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText minute_text;
-    private EditText seconde_text;
     private Button button_start;
-    private TextView arret,minuteur;
+    private TextView minuteur;
     private Minuteur minuteur_class = null;
+    private NumberPicker seconde_picker,minute_picker;
 
     private void bindViews() {
         button_start = findViewById(R.id.button_start);
-        minute_text = findViewById(R.id.minute);
-        seconde_text = findViewById(R.id.seconde);
-        arret = findViewById(R.id.arret);
         minuteur = findViewById(R.id.minuteur);
+        seconde_picker = findViewById(R.id.secondePicker);
+        seconde_picker.setMaxValue(59);
+        seconde_picker.setMinValue(0);
+        minute_picker = findViewById(R.id.minutePicker);
+        minute_picker.setMaxValue(59);
+        minute_picker.setMinValue(0);
     }
 
     @Override
@@ -29,35 +31,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         bindViews();
         button_start.setOnClickListener(this);
-
-        /*button_start.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                try {
-                    String var = minute_text.getText().toString();
-                    int progress = Integer.parseInt(var);
-                    while(progress != 0) {
-                        minuteur.setText(String.valueOf(progress));
-                        Thread.sleep(1000);
-                        progress --;
-                    };
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });*/
     }
 
     @Override
     public void onClick(View v) {
-        String var = minute_text.getText().toString();
+        int seconde = seconde_picker.getValue();
+        int minute = minute_picker.getValue();
         if (minuteur_class != null){
             minuteur_class.cancel(true);
             minuteur_class = null;
             button_start.setText("Start");
         } else {
-            minuteur_class = new Minuteur(this,var,minuteur);
+            minuteur_class = new Minuteur(this, minute,seconde,minuteur,minute_picker, seconde_picker,button_start);
             minuteur_class.execute();
-            //button_start.setText("Cancel");
+            button_start.setText("Cancel");
         }
     }
 
--- Project/app/src/main/java/com/example/rewan/chronometre/Minuteur.java
+++ Project/app/src/main/java/com/example/rewan/chronometre/Minuteur.java
@@ -2,6 +2,8 @@ package com.example.rewan.chronometre;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.widget.Button;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,25 +14,35 @@ import android.widget.Toast;
 public class Minuteur extends AsyncTask<Object, Integer, String> {
 
     private final Context context;
-    private final String time;
+    private final int time_sec,time_min;
     private final TextView minuteur;
+    private final NumberPicker minute_picker,seconde_picker;
+    private final Button button_start;
 
 
-    public Minuteur(Context context, String time, TextView minuteur) {
+    public Minuteur(Context context, int time_min , int time_sec, TextView minuteur, NumberPicker minute_picker,NumberPicker seconde_picker, Button button_start) {
         this.context = context;
-        this.time = time;
+        this.time_min = time_min;
+        this.time_sec = time_sec;
         this.minuteur = minuteur;
+        this.minute_picker = minute_picker;
+        this.seconde_picker = seconde_picker;
+        this.button_start = button_start;
     }
 
     @Override
     protected String doInBackground(Object[] params) {
-        int progress = Integer.parseInt(time);
+        int progress_sec = time_sec;
+        int progress_min = time_min;
+
+        if (progress_min != 0){
+            progress_sec = (progress_sec*progress_min)+60;
+        }
         try {
-            while(progress != 0) {
-                publishProgress(progress);
-                Thread.sleep(60000);
-                progress --;
-                publishProgress(progress);
+            while (progress_sec != 0) {
+                publishProgress(progress_sec);
+                Thread.sleep(1000);
+                progress_sec--;
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -44,11 +56,16 @@ public class Minuteur extends AsyncTask<Object, Integer, String> {
 
     @Override
     protected void onPostExecute(String s) {
+        minuteur.setText(String.valueOf(0));
+        minute_picker.setValue(0);
+        button_start.setText("Start");
+        Toast.makeText(context, "DRING..DRING..", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onProgressUpdate(Integer... values) {
         minuteur.setText(String.valueOf(values[0]));
+        minute_picker.setValue(values[0]);
     }
 
     @Override
